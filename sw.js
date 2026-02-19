b3b1c7375d1b135077db9e9ab4729ca76d266132
a01029800119-creator <a01029800119@gmail.com>
2026-02-19 15:03:44 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+// 최소 PWA 설치 인식 + 오프라인 기본 캐시(간단 버전)
+const CACHE = "bangcho-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
